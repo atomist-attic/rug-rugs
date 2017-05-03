742ae513d601f2001c329f89ef4af3118731cbb0
Christian Dupuis <cd@atomist.com>
2017-05-03 09:33:27 +0200

    Delint

--- .atomist/editors/BumpVersion.ts
+++ .atomist/editors/BumpVersion.ts
@@ -1,7 +1,7 @@
-import { EditProject } from '@atomist/rug/operations/ProjectEditor';
-import { Project } from '@atomist/rug/model/Project';
-import { Pattern } from '@atomist/rug/operations/RugOperation';
-import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
+import { Project } from "@atomist/rug/model/Project";
+import { Editor, Parameter, Tags } from "@atomist/rug/operations/Decorators";
+import { EditProject } from "@atomist/rug/operations/ProjectEditor";
+import { Pattern } from "@atomist/rug/operations/RugOperation";
 
 /**
  * Increment the version in manifest.yml
@@ -17,39 +17,41 @@ export class BumpVersion implements EditProject {
         validInput: "major | minor | patch",
         minLength: 1,
         maxLength: 5,
-        required: false
+        required: false,
     })
-    component: string = "patch";
+    public component: string = "patch";
 
-    edit(project: Project) {
-        let versionRegex = /version: "?(\d+)\.(\d+)\.(\d+)"?/;
-        let manifest = project.findFile(".atomist/manifest.yml");
+    public edit(project: Project) {
+        const versionRegex = /version: "?(\d+)\.(\d+)\.(\d+)"?/;
+        const manifest = project.findFile(".atomist/manifest.yml");
         if (manifest == null) {
             // not a rug archive
             return;
         }
-        let versionMatch = versionRegex.exec(manifest.content);
+        const versionMatch = versionRegex.exec(manifest.content);
         if (!versionMatch) {
-            throw "Unable to parse current version. I can only increment a nice simple 1.2.3 format. But I see: " + manifest.content
+            throw new Error(
+               // tslint:disable-next-line:max-line-length
+               `Unable to parse current version. I can only increment a nice simple 1.2.3 format. But I see: ${manifest.content}`);
         }
 
         let major = parseInt(versionMatch[1]);
         let minor = parseInt(versionMatch[2]);
         let patch = parseInt(versionMatch[3]);
-        if (this.component == "major") {
+        if (this.component === "major") {
             major = major + 1;
             minor = 0;
             patch = 0;
-        } else if (this.component == "minor") {
+        } else if (this.component === "minor") {
             minor = minor + 1;
             patch = 0;
-        } else if (this.component == "patch") {
+        } else if (this.component === "patch") {
             patch = patch + 1;
         } else {
-            throw `Unknown version component '${this.component}'. Should be major|minor|patch`;
+            throw new Error(`Unknown version component '${this.component}'. Should be major|minor|patch`);
         }
 
-        let newContent = manifest.content.replace(versionRegex, `version: "${major}.${minor}.${patch}"`)
+        const newContent = manifest.content.replace(versionRegex, `version: "${major}.${minor}.${patch}"`);
         manifest.setContent(newContent);
     }
 }
